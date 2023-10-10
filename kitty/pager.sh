cde93472934f203603f39b17d8f0c84213da6929
jk <jakobhkrohn@gmail.com>
2023-10-10 13:47:06 +0200

    kitty pager: start on bottom

--- kitty/pager.sh
+++ kitty/pager.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
-
-if [ "$#" -eq 3 ]; then
-    INPUT_LINE_NUMBER=${1:-0}
-    CURSOR_LINE=${2:-1}
-    CURSOR_COLUMN=${3:-1}
-    AUTOCMD_TERMCLOSE_CMD="call cursor(max([0,${INPUT_LINE_NUMBER}-1])+${CURSOR_LINE}, ${CURSOR_COLUMN})"
-else
+# Uncomment to start on the top
+# if [ "$#" -eq 3 ]; then
+#     INPUT_LINE_NUMBER=${1:-0}
+#     CURSOR_LINE=${2:-1}
+#     CURSOR_COLUMN=${3:-1}
+#     AUTOCMD_TERMCLOSE_CMD="call cursor(max([0,${INPUT_LINE_NUMBER}-1])+${CURSOR_LINE}, ${CURSOR_COLUMN})"
+# else
     AUTOCMD_TERMCLOSE_CMD="normal G"
-fi
+# fi
 
 exec nvim 63<&0 0</dev/null \
     -u NONE \
