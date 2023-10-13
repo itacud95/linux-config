8dce2f7e65426012675ca8dbcdf10dfb5ef436fd
jk <jakobhkrohn@gmail.com>
2023-10-13 10:01:23 +0200

    Add applications

---
+++ applications/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# update
+yay
+
+# install packages
+yay -S \
+speedcrunch \
+7-zip-full \
+cheese \
+ferdium-bin \
+flameshot \
+keyd \
+kitty \
+signal-desktop-beta-bin \
+
+
+# work related
+yay -S \
+android-apktool \
+android-sdk-cmdline-tools-latest \
+bundletool \
+clang-format-static-bin 
+cmake \
+slack-desktop \
+wireguard-tools 
+valgrind \
+
+# argcomplete
+yay -S python-argcomplete
+sudo activate-global-python-argcomplete --dest /usr/share/bash-completion/completions/
