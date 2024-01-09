4442d3173cf9406d16953935323c5bb27b3d15cf
jakob <jakobhkrohn@gmail.com>
2024-01-09 16:00:30 +0100

    add lua and json LSP

--- nvim/lsp.lua
+++ nvim/lsp.lua
@@ -2,9 +2,14 @@ local lspconfig = require('lspconfig')
 
 lspconfig.clangd.setup {}
 
+-- yay -S lua-language-server
+lspconfig.lua_ls.setup {}
+
+-- yay -S vscode-langservers-extracted
+lspconfig.jsonls.setup{}
+
 -- lspconfig.cmake.setup {
 --     settings = {
 --         filetypes = { "cmake", "CMakeLists.txt" }
 --     }
 -- }
-
